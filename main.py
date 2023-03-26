5a65136f2662824369a4e6bf6fd41174d127cbc4
tackpapa <cance0311@gmail.com>
2023-03-26 19:43:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import nltk
 nltk.download('punkt')
 
 # st.markdown('<style> .css-1v0mbdj {margin:0 auto; width:50%; </style>', unsafe_allow_html=True)
-
+port = 10000
+st.set_option('server.port', port)
 
 st.title('Article Summarizer')
 
-url = st.text_input('', placeholder='Paste the URL of the article amd press Enter')
+url = st.text_input(
+    '', placeholder='Paste the URL of the article amd press Enter')
 
 if url:
     try:
@@ -21,33 +23,30 @@ if url:
 
         img = article.top_image
         st.image(img)
-        
 
         title = article.title
         st.subheader(title)
-        
 
         authors = article.authors
         st.text(','.join(authors))
-        
+
         article.nlp()
 
         keywords = article.keywords
         st.subheader('Keywords:')
         st.write(', '.join(keywords))
-        
-        tab1, tab2= st.tabs(["Full Text", "Summary"])
+
+        tab1, tab2 = st.tabs(["Full Text", "Summary"])
         with tab1:
             txt = article.text
             txt = txt.replace('Advertisement', '')
             st.write(txt)
-        
+
         with tab2:
             st.subheader('Summary')
             summary = article.summary
             summary = summary.replace('Advertisement', '')
             st.write(summary)
-        
-        
+
     except:
         st.error('Sorry something went wrong')
