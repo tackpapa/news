052983651ba00b827b9c3d1ada374d46e5e4c468
tackpapa <cance0311@gmail.com>
2023-03-26 20:17:48 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import newspaper
 import nltk
+import streamlit.components.v1 as components
 
 nltk.download('punkt')
 
 # st.markdown('<style> .css-1v0mbdj {margin:0 auto; width:50%; </style>', unsafe_allow_html=True)
-port = 10000
-st.set_option('server.port', port)
-st.set_option('server.address', '0.0.0.0')
+# port = 10000
+# st.set_option('server.port', port)
+# st.set_option('server.address', '0.0.0.0')
 
 st.title('Article Summarizer')
 
